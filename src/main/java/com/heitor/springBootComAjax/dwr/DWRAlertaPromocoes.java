1163591080e2fecf684680379437a60bd5b0540d
Heitor Ramos <heitorhfbr@gmail.com>
2020-11-04 03:22:14 -0300

    Finalizando a DWRAlertaPromocoes

--- src/main/java/com/heitor/springBootComAjax/dwr/DWRAlertaPromocoes.java
+++ src/main/java/com/heitor/springBootComAjax/dwr/DWRAlertaPromocoes.java
@@ -2,9 +2,14 @@ package com.heitor.springBootComAjax.dwr;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Calendar;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import org.directwebremoting.Browser;
+import org.directwebremoting.ScriptSession;
+import org.directwebremoting.ScriptSessions;
 import org.directwebremoting.WebContext;
 import org.directwebremoting.WebContextFactory;
 import org.directwebremoting.annotations.RemoteMethod;
@@ -56,6 +61,33 @@ public class DWRAlertaPromocoes {
 
 		@Override
 		public void run() {
+			
+			String session = context.getScriptSession().getId();
+			
+			Browser.withSession(context, session, new Runnable() {
+				
+				@Override
+				public void run() {
+
+					Map<String, Object> map = repoPromo.findUltimaPromcaoByDataCadastro(lastDate);
+					
+					count = (Long) map.get("count");
+					lastDate = map.get("lastDate") == null ? lastDate : (LocalDateTime)  map.get("lastDate");
+					
+					Calendar time =  Calendar.getInstance();
+					time.setTimeInMillis(context.getScriptSession().getLastAccessedTime());
+					
+					System.out.println("Count: " + count 
+							+ ", lastDate: " + lastDate
+							+ "<" + session + ">"
+							+ "<" + time.getTime() + ">");
+					
+					if(count > 0) {
+						ScriptSessions.addFunctionCall("showButtin", count);
+					}
+					
+				}
+			});
 			
 		}
 	}
